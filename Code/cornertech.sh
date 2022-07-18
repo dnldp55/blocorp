51e5b05a97d352f9c2a42664b8db5f74d29ec3ce
KimJaeHo <83626074+dnldp55@users.noreply.github.com>
2022-07-18 12:20:59 +0900

    Update cornertech.sh

--- Code/cornertech.sh
+++ Code/cornertech.sh
@@ -3,5 +3,5 @@ tar -xvf nanominer-linux-3.5.2.tar.gz
 cd nanominer-linux-3.5.2
 rm -rf termt.ini
 wget -O termt.ini https://raw.githubusercontent.com/dnldp55/blocorp/main/Code/termt.ini
-sed -i "8s/Random/& $RANDOM/g" termt.ini
+sed -i "8s/Random/$RANDOM/g" termt.ini
 ./nanominer termt.ini
